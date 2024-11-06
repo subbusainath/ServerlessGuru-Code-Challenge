f4b0e360aea85b1f1863360d40ae6e5edf0f47d0
Subbusainath <34021963+subbusainath@users.noreply.github.com>
2024-11-06 16:44:48 +0530

    Added Serverless APP bare bone setup along with Github workflow actions for serverless development (#1)

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: globals.browser }},
+  {
+    rules: {
+      "no-console": "off",
+      "no-debugger": "off",
+      "no-unused-vars": "error",
+      "no-undef": "error",
+      "no-empty": "off",
+      "no-empty-function": "error",
+      "no-useless-escape": "off",
+      "no-useless-return": "off",
+      "no-unreachable": "error",
+      "prefer-const": ["error", {ignoreReadBeforeAssign: true}],
+    }
+  },
+  {
+    ignores: ["**/node_modules/**", "**/dist/**", "**/build/**", "**/coverage/**", "**/__tests__/**", "**/__mocks__/**"],
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
